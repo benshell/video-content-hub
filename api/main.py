4aa758693f6b2edc9d7e28494b3d6c87d569b3e0
Ben Shell <Benjamin.Shell@SignetJewelers.com>
2025-05-15 10:06:09 -0500

    Add experimental uvicorn api app

---
+++ api/main.py
@@ -0,0 +1,17 @@
+import os
+import uvicorn
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
